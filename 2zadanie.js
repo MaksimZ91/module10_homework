4bc0d0b2680cebd1ab247602be92d8bf7415bd71
ytokarevskaya <lan2214@yandex.ru>
2020-10-15 00:28:49 +0300

    Проверено

--- 2zadanie.js
+++ 2zadanie.js
@@ -5,17 +5,21 @@ c=true
 d=[1,2,3]
 i=null
 let f
-X=2 ;
-switch (typeof X) {
+let x = 2;
+switch (typeof x) {
    case "number":
          console.log("X-число");
          break
-   case X= "string":
+   case "string":
          console.log("X-строка");
          break
-   case X= "boolean":
+   case "boolean":
         console.log("X-логика");
         break
    default:
   console.log('«Тип x не определён»');
 }
+
+// Всё верно, но снова есть замечания:
+// 1. Всегда используйте ключевые слова при объявлении переменных: let или const.
+// 2. Строка X= в директиве case - лишняя
